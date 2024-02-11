7efef8c16aecc89dc604d709678cf6bf452fe1d6
devkumaoni <142296558+devkumaoni@users.noreply.github.com>
2024-02-11 10:21:57 +0530

    update url

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,6 @@
 const Dashboard = document.querySelector("#dashboard");
 
-fetch("https://employee-api-beta.vercel.app/fetch", { method: "get" })
+fetch("https://employee-server-ci8r.onrender.com/fetch", { method: "get" })
   .then((res) => res.json())
   .then((data) => {
     data.map(
