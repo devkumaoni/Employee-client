3224b4cb048dac56fdf79c07a7f81a406362f969
devkumaoni <142296558+devkumaoni@users.noreply.github.com>
2024-02-11 13:12:54 +0530

    update  index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("button_Submit_From");
+
+  btn.addEventListener("click", () => {
+    let data = document.querySelectorAll(".post_data");
+    data = Array.from(data).map((d) => {
+      return { [d.name]: d.value };
+    });
+
+    fetch("https://employee-server-a1wz.onrender.com/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status == 201) {
+          console.log("Success");
+        }
+      })
+      .catch((err) => console.log("Error"));
+  });
+});
